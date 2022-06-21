a54dc4f2f410b008a9355e741ceabebaeb3c95ee
vbmTb0b9 <itthijit@gmail.com>
2022-06-21 11:32:02 +0700

    change prefix

--- sd.config.cjs
+++ sd.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
     css: {
       transformGroup: 'css',
       buildPath: 'tokens/',
-      prefix: 'beci',
+      prefix: '3plus',
       files: [
         {
           destination: 'variables.css',
