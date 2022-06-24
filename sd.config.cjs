e821dfe454efca6ca5b219e9d9cf4963e5102337
vbmTb0b9 <itthijit@gmail.com>
2022-06-24 14:32:34 +0700

    add ios

--- sd.config.cjs
+++ sd.config.cjs
@@ -32,5 +32,98 @@ module.exports = {
         format: "android/colors"
       }]
     },
+    ios: {
+      transformGroup: "ios",
+      buildPath: "tokens/ios/",
+      files: [{
+        destination: "StyleDictionaryColor.h",
+        format: "ios/colors.h",
+        className: "StyleDictionaryColor",
+        type: "StyleDictionaryColorName",
+        filter: {
+          attributes: {
+            category: "color"
+          }
+        }
+      },{
+        destination: "StyleDictionaryColor.m",
+        format: "ios/colors.m",
+        className: "StyleDictionaryColor",
+        type: "StyleDictionaryColorName",
+        filter: {
+          attributes: {
+            category: "color"
+          }
+        }
+      },{
+        destination: "StyleDictionarySize.h",
+        format: "ios/static.h",
+        className: "StyleDictionarySize",
+        type: "float",
+        filter: {
+          attributes: {
+            category: "size"
+          }
+        }
+      },{
+        destination: "StyleDictionarySize.m",
+        format: "ios/static.m",
+        className: "StyleDictionarySize",
+        type: "float",
+        filter: {
+          attributes: {
+            category: "size"
+          }
+        }
+      }]
+    },
+    ios-swift: {
+      transformGroup: "ios-swift",
+      buildPath: "tokens/ios-swift/",
+      files: [{
+        destination: "StyleDictionary+Class.swift",
+        format: "ios-swift/class.swift",
+        className: "StyleDictionaryClass",
+        filter: {}
+      },{
+        destination: "StyleDictionary+Enum.swift",
+        format: "ios-swift/enum.swift",
+        className: "StyleDictionaryEnum",
+        filter: {}
+      },{
+        destination: "StyleDictionary+Struct.swift",
+        format: "ios-swift/any.swift",
+        className: "StyleDictionaryStruct",
+        filter: {},
+        options: {
+          imports: "SwiftUI",
+          objectType: "struct",
+          accessControl: "internal"
+        }
+      }]
+    },
+    ios-swift-separate-enums: {
+      transformGroup: "ios-swift-separate",
+      buildPath: "tokens/ios-swift/",
+      files: [{
+        destination: "StyleDictionaryColor.swift",
+        format: "ios-swift/enum.swift",
+        className: "StyleDictionaryColor",
+        filter: {
+          attributes: {
+            category: "color"
+          }
+        }
+      },{
+        destination: "StyleDictionarySize.swift",
+        format: "ios-swift/enum.swift",
+        className: "StyleDictionarySize",
+        filter: {
+          attributes: {
+            category: "size"
+          }
+        }
+      }]
+    }
   },
 };
