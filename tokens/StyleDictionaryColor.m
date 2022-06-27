891701c1225921da150e079656cdbb927d52bfaa
vbmTb0b9 <itthijit@gmail.com>
2022-06-27 11:02:19 +0700

    test

---
+++ tokens/StyleDictionaryColor.m
@@ -0,0 +1,40 @@
+
+//
+// StyleDictionaryColor.m
+//
+
+// Do not edit directly
+// Generated on Mon, 27 Jun 2022 04:02:09 GMT
+
+
+#import "StyleDictionaryColor.h"
+
+@implementation StyleDictionaryColor
+
++ (UIColor *)color:(StyleDictionaryColorName)colorEnum{
+  return [[self values] objectAtIndex:colorEnum];
+}
+
++ (NSArray *)values {
+  static NSArray* colorArray;
+  static dispatch_once_t onceToken;
+
+  dispatch_once(&onceToken, ^{
+    colorArray = @[
+[UIColor colorWithRed:0.051f green:0.349f blue:0.949f alpha:1.000f],
+[UIColor colorWithRed:0.239f green:0.478f blue:0.961f alpha:1.000f],
+[UIColor colorWithRed:0.463f green:0.643f blue:1.000f alpha:1.000f],
+[UIColor colorWithRed:1.000f green:0.000f blue:0.000f alpha:1.000f],
+[UIColor colorWithRed:0.192f green:0.412f blue:0.039f alpha:1.000f],
+[UIColor colorWithRed:0.996f green:0.996f blue:0.278f alpha:1.000f],
+[UIColor colorWithRed:1.000f green:0.000f blue:0.910f alpha:1.000f],
+[UIColor colorWithRed:0.545f green:0.545f blue:0.545f alpha:1.000f],
+[UIColor colorWithRed:0.400f green:0.000f blue:0.561f alpha:1.000f],
+[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:1.000f]
+    ];
+  });
+
+  return colorArray;
+}
+
+@end
---
+++ tokens/StyleDictionaryColor.swift
@@ -0,0 +1,23 @@
+
+//
+// StyleDictionaryColor.swift
+//
+
+// Do not edit directly
+// Generated on Mon, 27 Jun 2022 04:02:09 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryColor {
+    public static let blue300 = UIColor(red: 0.463, green: 0.643, blue: 1.000, alpha: 1)
+    public static let blue400 = UIColor(red: 0.239, green: 0.478, blue: 0.961, alpha: 1)
+    public static let blue500 = UIColor(red: 0.051, green: 0.349, blue: 0.949, alpha: 1)
+    public static let ctaBackground = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let gray = UIColor(red: 0.545, green: 0.545, blue: 0.545, alpha: 1)
+    public static let pink31 = UIColor(red: 1.000, green: 0.000, blue: 0.910, alpha: 1)
+    public static let purple = UIColor(red: 0.400, green: 0.000, blue: 0.561, alpha: 1)
+    public static let red = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let tea = UIColor(red: 0.192, green: 0.412, blue: 0.039, alpha: 1)
+    public static let yellow30 = UIColor(red: 0.996, green: 0.996, blue: 0.278, alpha: 1)
+}
