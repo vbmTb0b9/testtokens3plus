4f238ec677125217cff3ea1373e7f0a9051e1aa6
vbmTb0b9 <itthijit@gmail.com>
2022-06-27 14:16:53 +0700

    edir

--- sd.config.cjs
+++ sd.config.cjs
@@ -77,53 +77,5 @@ module.exports = {
         }
       }]
     },
-    ios-swift: {
-      transformGroup: "ios-swift",
-      buildPath: "tokens/ios-swift/",
-      files: [{
-        destination: "StyleDictionary+Class.swift",
-        format: "ios-swift/class.swift",
-        className: "StyleDictionaryClass",
-        filter: {}
-      },{
-        destination: "StyleDictionary+Enum.swift",
-        format: "ios-swift/enum.swift",
-        className: "StyleDictionaryEnum",
-        filter: {}
-      },{
-        destination: "StyleDictionary+Struct.swift",
-        format: "ios-swift/any.swift",
-        className: "StyleDictionaryStruct",
-        filter: {},
-        options: {
-          imports: "SwiftUI",
-          objectType: "struct",
-          accessControl: "internal"
-        }
-      }]
-    },
-    ios-swift-separate-enums: {
-      transformGroup: "ios-swift-separate",
-      buildPath: "tokens/ios-swift/",
-      files: [{
-        destination: "StyleDictionaryColor.swift",
-        format: "ios-swift/enum.swift",
-        className: "StyleDictionaryColor",
-        filter: {
-          attributes: {
-            category: "color"
-          }
-        }
-      },{
-        destination: "StyleDictionarySize.swift",
-        format: "ios-swift/enum.swift",
-        className: "StyleDictionarySize",
-        filter: {
-          attributes: {
-            category: "size"
-          }
-        }
-      }]
-    }
   },
 };
