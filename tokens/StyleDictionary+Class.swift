7ed1c00d8537414ef94b554a42dd0ef17310b2c0
Itthijit Manjit <itthijitm@becworld.com>
2022-06-27 07:24:14 +0000

    chore: style-dictionary output

---
+++ tokens/StyleDictionary+Class.swift
@@ -0,0 +1,34 @@
+
+//
+// StyleDictionary+Class.swift
+//
+
+// Do not edit directly
+// Generated on Mon, 27 Jun 2022 07:24:13 GMT
+
+
+import UIKit
+
+public class StyleDictionaryClass {
+    public static let body = 32px
+    public static let colorBlue300 = UIColor(red: 0.463, green: 0.643, blue: 1.000, alpha: 1)
+    public static let colorBlue400 = UIColor(red: 0.239, green: 0.478, blue: 0.961, alpha: 1)
+    public static let colorBlue500 = UIColor(red: 0.051, green: 0.349, blue: 0.949, alpha: 1)
+    public static let colorCtaBackground = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorGray = UIColor(red: 0.545, green: 0.545, blue: 0.545, alpha: 1)
+    public static let colorPink31 = UIColor(red: 1.000, green: 0.000, blue: 0.910, alpha: 1)
+    public static let colorPurple = UIColor(red: 0.400, green: 0.000, blue: 0.561, alpha: 1)
+    public static let colorRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorTea = UIColor(red: 0.192, green: 0.412, blue: 0.039, alpha: 1)
+    public static let colorYellow30 = UIColor(red: 0.996, green: 0.996, blue: 0.278, alpha: 1)
+    public static let fontFamMainFont = DB Heavent
+    public static let fontWeightBold = bold
+    public static let fontWeightMedium = Med
+    public static let lineHeighLh100 = 100%
+    public static let roundLg = 16px
+    public static let roundMd = 8px
+    public static let roundSm = 4px
+    public static let sizeLg = 32px
+    public static let sizeMd = 16px
+    public static let sizeSm = 8px
+}
---
+++ tokens/StyleDictionaryColor.kt
@@ -0,0 +1,25 @@
+
+
+// Do not edit directly
+// Generated on Mon, 27 Jun 2022 07:24:13 GMT
+
+
+
+package StyleDictionaryColor;
+
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object StyleDictionaryColor {
+  val colorBlue300 = Color(0xff76a4ff)
+  val colorBlue400 = Color(0xff3d7af5)
+  val colorBlue500 = Color(0xff0d59f2)
+  val colorCtaBackground = Color(0xff000000)
+  val colorGray = Color(0xff8b8b8b)
+  val colorPink31 = Color(0xffff00e8)
+  val colorPurple = Color(0xff66008f)
+  val colorRed = Color(0xffff0000)
+  val colorTea = Color(0xff31690a)
+  val colorYellow30 = Color(0xfffefe47)
+}
